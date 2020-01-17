4d44f972871b31079a782a21be6ba3a3f19ad2bf
yusuke39 <hiranoyusuke39@gmail.com>
2020-01-17 11:07:54 +0900

    js追加

--- main.js
+++ main.js
@@ -1,48 +1,74 @@
-// //動くものクラスの定義
-// var Movable = function(x,y){
-//   this.pos = {
-//     x: x,
-//     y: y
-//   };
-//   this.move = function(x,y){
-//     this.pos.x += x;
-//     this.pos.y += y;
-//   };
-// }
+//コンポーネントのルートノード
+var nodeApp = document.querySelector('#app');
 
-// //ballオブジェクトを格納する空の配列を用意する
-// var ball = [];
-// //100個分の繰り返し
-// for(var i=0; i<=100; i++){
-//   ball[i] = new Movable(
-//     Math.floor(Math.random() * window.innerWidth),
-//     Math.floor(Math.random() * window.innerHeight)
-//   );
-// }
+//チェックボックスのイベントハンドラを登録
+var nodeCheckbox = nodeApp.querySelectorAll('input[type="checkbox"]');
+nodeCheckbox[0].addEventListener('change', onCheckChanged, false);
+nodeCheckbox[1].addEventListener('change', onCheckChanged, false);
 
-// console.log(Math.floor(Math.random() * window.innerWidth))
-// console.log( Math.floor(Math.random() * window.innerHeight))
+//チェック状態変更イベントハンドラ
+function onCheckChanged(event){
 
-// //ボールをブラウザに描画する
-// for(var i=0; i<=100; i++){
-//   document.write('<div class="ball" style="top:' + ball[i].pos.y + 'px;left:' + ball[i].pos.x +  'px;">●</div>');
-// }
+  var nodeItems = nodeApp.querySelectorAll('.item');//商品ノードのリスト
+  var nodeCount = nodeApp.querySelector('.count');//表示件数のノード
+  var count = nodeItems.length;//表示件数
 
-// var ball = new Vue({
-//   el: '#ball',
-//   data: {
-//     pos: {x: 0, y:0},
-//     radius: 20
-//   }
-// });
-
-// ball.move(100, 100);
-// ball.radius = 40;
-
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    show: true
+  //全ての商品ノードを一旦表示する
+  for(var i=0; i<nodeItems.length; i++){
+    showNode(nodeItems[i]);
   }
-});
+
+  //セール対象のチェックが付いている場合
+  if(nodeCheckbox[0].checked){
+    //全ての商品ノードを操作
+    for(var i=0; i<nodeItems.length; i++){
+      //セール対象の商品ではない場合
+      if(!isSaleItem(nodeItems[i])){
+        //この商品を非表示にする
+        hideNode(nodeItems[i]);
+        //件数カウントを減らす
+        count--;
+      }
+    }
+  }
+
+  //送料無料のチェックが付いている場合
+  if(nodeCheckbox[1].checked){
+    //全ての商品ノードを操作
+    for(var i=0; i<nodeItems.length; i++){
+      //送料無料の商品ではない場合
+      if(!isDelvFreeItem(nodeItems[i])){
+        //この商品を非表示にする
+        hideNode(nodeItems[i]);
+        //件数カウントを減らす
+        count--;
+      }
+    }
+  }
+
+  //件数を更新
+  nodeCount.textContent = count + '件';
+
+  function isSaleItem(nodeItem){
+    var node = nodeItem.querySelector('.status');
+    return(node && node.textContent == 'SALE');
+  }
+
+  //送料無料かどうかを判定する関数
+  function isDelvFreeItem(nodeItem){
+    var node = nodeItem.querySelector('.shipping-fee');
+    return (node && node.textContent == '送料無料');
+  }
+
+  //ノードを非表示にする関数
+  function hideNode(node){
+    node.setAttribute('style','display:none');
+  }
+
+
+  //ノードを表示する関数
+  function showNode(node){
+    node.removeAttribute('style');
+  }
+
+}
